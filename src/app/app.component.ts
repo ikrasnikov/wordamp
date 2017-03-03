060f29bd9d513c454ab5c28e31c9d5014e35ee7a
i.krasnikov <i.krasnikov@studutube.nl>
2017-03-03 17:48:38 +0200

    Change rout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'wordcamp',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
